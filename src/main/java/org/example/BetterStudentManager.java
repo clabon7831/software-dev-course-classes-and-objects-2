aeb5d6621398690759836ae3d94d527e13c5b68c
clabon7831 <chris.labon@gmail.com>
2026-09-24 18:53:03 -0500

    Completed BetterStudentManager exercise

--- src/main/java/org/example/BetterStudentManager.java
+++ src/main/java/org/example/BetterStudentManager.java
@@ -12,10 +12,22 @@ public class BetterStudentManager {
 
     public void addStudent(String name, double grade) {
         // This method should add a new student to the list of students
+
+        Student student = new Student();
+        student.name = name;
+        student.grade = grade;
+        students.add(student);
     }
 
     public void removeStudent(String name) {
         // This method should remove the student with the given name from the list of students
+
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).name.equals(name)) {
+                students.remove(i);
+                return;
+            }
+        }
     }
 
     public String getStudentList() {
@@ -29,6 +41,17 @@ public class BetterStudentManager {
         // looping through the list of students
         //
         // Replace the following line with your implementation
-        return null;
+
+        StringBuilder result = new StringBuilder();
+
+        for (Student student : students) {
+            result.append(student.name)
+                    .append(" ")
+                    .append(student.grade)
+                    .append("\n");
+        }
+
+        return result.toString();
     }
+
 }
